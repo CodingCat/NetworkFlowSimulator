359665052b77f97a8d16088cb82b78b25604defa
CodingCat <zhunan@zhunan-2.local>
2013-07-08 22:12:13 -0400

    change SimulationEngine to TreeMap and develop cancelEvent method

--- scalaflowsim/src/main/scala/simengine/SimulationEngine.scala
+++ scalaflowsim/src/main/scala/simengine/SimulationEngine.scala
@@ -1,32 +1,40 @@
 package scalasim.simengine
 
-import scala.collection.mutable.PriorityQueue
-import network.events.StartNewFlowEvent
-import scala.collection.mutable
-import network.topo.Link
-import network.data.Flow
+import scala.collection.immutable.TreeMap
 
 
 object SimulationEngine {
 
-  implicit object EventOrder extends Ordering[Event] {
-    def compare(x : Event, y : Event) = x.getTimeStamp > y.getTimeStamp match {
-      case true => -1
+  implicit object KeyOrder extends Ordering[Double] {
+    def compare(x : Double, y : Double) = x > y match {
+      case true => 1
       case false => {
-        x.getTimeStamp == y.getTimeStamp match {
+        x == y match {
           case true => 0
-          case false => 1
+          case false => -1
         }
       }
     }
   }
 
-  val eventqueue = new PriorityQueue[Event]()(EventOrder)
+  var currentTime : Double = 0.0
 
-  def run() = for (event <- eventqueue) event.process
+  var eventqueue = new TreeMap[Double, Event]()(KeyOrder)
 
-  def addEvent(e : Event) = eventqueue += e
+  def run() {
+    for (event <- eventqueue.values) {
+      event.process
+      currentTime = event.getTimeStamp
+    }
+  }
 
+  def addEvent(e : Event) {
+    eventqueue = eventqueue + (e.getTimeStamp -> e)
+  }
+
+  def cancelEvent(e : Event) {
+    eventqueue = eventqueue - e.getTimeStamp
+  }
 }
 
 
--- scalaflowsim/src/test/scala/SimEngineSuite.scala
+++ scalaflowsim/src/test/scala/SimEngineSuite.scala
@@ -18,10 +18,23 @@ class SimEngineSuite extends FunSuite{
     SimulationEngine.addEvent(e2)
     SimulationEngine.addEvent(e3)
     var r = -1.0
-    for (e <- SimulationEngine.eventqueue) {
+    for (e <- SimulationEngine.eventqueue.values) {
       assert((r < e.asInstanceOf[DummySingleEntity].getTimeStamp) === true)
       r = e.asInstanceOf[DummySingleEntity].getTimeStamp
     }
   }
 
+  test ("Events can be rescheduled") {
+    val e1 = new DummySingleEntity("e1", 10)
+    val e2 = new DummySingleEntity("e2", 5)
+    val e3 = new DummySingleEntity("e3", 20)
+    SimulationEngine.addEvent(e1)
+    SimulationEngine.addEvent(e2)
+    SimulationEngine.addEvent(e3)
+    SimulationEngine.cancelEvent(e3)
+    e3.setTimeStamp(1)
+    SimulationEngine.addEvent(e3)
+    assert(SimulationEngine.eventqueue.size === 3)
+    assert(SimulationEngine.eventqueue.head._2 === e3)
+  }
 }
