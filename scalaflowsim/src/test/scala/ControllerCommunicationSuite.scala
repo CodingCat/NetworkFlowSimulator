e99fbf35e87e88bec757d0112b753ae45906d92f
CodingCat <zhunansjtu@gmail.com>
2013-07-18 10:58:52 -0400

    add OpenFlowMsgDispatcher

---
+++ scalaflowsim/src/test/scala/ControllerCommunicationSuite.scala
@@ -0,0 +1,12 @@
+package scalasim.test
+
+import org.scalatest.FunSuite
+import simengine.utils.Logging
+
+
+class ControllerCommunicationSuite extends FunSuite with Logging {
+  test("the router can communicate with the controller ") {
+
+  }
+
+}
