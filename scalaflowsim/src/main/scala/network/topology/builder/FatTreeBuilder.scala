5ac2a2ef9526dbfd7a7f631f644713127e812b38
CodingCat <zhunansjtu@gmail.com>
2013-11-10 15:32:35 -0500

    link aggregate and core

--- scalaflowsim/src/main/scala/network/topology/builder/FatTreeBuilder.scala
+++ scalaflowsim/src/main/scala/network/topology/builder/FatTreeBuilder.scala
@@ -12,32 +12,12 @@ class FatTreeBuilder (private val podnum: Int = 4,
   val edge_sws_idx = 0 until (podnum / 2)
   val hosts_idx = 2 until (podnum / 2 + 2)
 
-  /*
-  def buildRack(router: Router, hosts : HostContainer, startIdx : Int, endIdx : Int) {
-    try {
-      var locallinkBandwidth = XmlParser.getDouble("scalasim.topology.locallinkrate", 100.0)
-      if (router.ip_addr.length == 0) throw new RuntimeException("Engress Router hasn't got ipaddress")
-      for (i <- startIdx to endIdx) {
-        if (hosts(i).ip_addr.length == 0)  {
-          throw new RuntimeException("Hosts haven't got ipaddress, the idx is " + i)
-        }
-        val newlink = new Link(hosts(i), router, locallinkBandwidth)
-        hosts(i).interfacesManager.registerOutgoingLink(newlink)
-        router.interfacesManager.registerIncomeLink(newlink)
-        GlobalDeviceManager.addNewNode(hosts(i).ip_addr(0), hosts(i))
-      }
-      GlobalDeviceManager.addNewNode(router.ip_addr(0), router)
-    }
-    catch {
-      case ex : RuntimeException => throw ex
-    }
-  }
 
-   */
-
-  def buildNetwork(aggRouters : RouterContainer, edgeRouters : RouterContainer,
+  def buildNetwork(coreRouters: RouterContainer, aggRouters : RouterContainer,
+                   edgeRouters : RouterContainer,
                    hosts: HostContainer) {
     for (pod_idx <- pods) {
+      //connect the eage to each aggregate switch and make the connection within the rack
       for (edge_idx <- edge_sws_idx) {
         for (host_idx <- hosts_idx) {
           val newlink = new Link(hosts(host_idx - 2), edgeRouters(edge_idx), linkspeed)
@@ -56,6 +36,19 @@ class FatTreeBuilder (private val podnum: Int = 4,
         }
       }
 
+      for (agg_idx <- agg_sws_idx) {
+       // val c_index = agg_idx - podnum / 2 + 1
+
+        for (core_idx <- core_sws_idx) {
+          val newlink = new Link(coreRouters(core_idx), aggRouters(agg_idx - podnum / 2),
+            linkspeed)
+          aggRouters(agg_idx - podnum / 2).interfacesManager.registerOutgoingLink(newlink)
+          coreRouters(core_idx - 1).interfacesManager.registerOutgoingLink(newlink)
+          GlobalDeviceManager.addNewNode(coreRouters(core_idx - 1).ip_addr(0),
+            coreRouters(core_idx - 1))
+
+        }
+      }
     }
   }
 }
